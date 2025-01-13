8e62063d5660e1c25e9a13dbc366552f513f8cd2
0xmycf <mycf.mycf.mycf@gmail.com>
2025-01-13 02:20:01 +0100

    nixify

--- flake.nix
+++ flake.nix
@@ -1,23 +1,41 @@
 {
   inputs = {
     utils.url = "github:numtide/flake-utils";
-    nixpkgs.url =  "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
   };
-  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          # stuff i use inside rust / cargo
-          rustc cargo rustfmt 
-          # linter
-          clippy
-          # lsp stuff
-          rust-analyzer
-        ];
-      };
-    }
-  );
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        brot = pkgs.rustPlatform.buildRustPackage {
+          pname = "brot";
+          version = "1.0.0";
+          cargoLock.lockFile = ./Cargo.lock;
+          src = pkgs.lib.cleanSource ./.;
+        };
+      in {
+        packages = {
+          brot = brot;
+          default = brot;
+        };
+
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # stuff i use inside rust / cargo
+            rustc
+            cargo
+            rustfmt
+            # linter
+            clippy
+            # lsp stuff
+            rust-analyzer
+          ];
+        };
+      }
+    );
 }
